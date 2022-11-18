f6578fde073a29afd8dedf16002c268f2b741ea4
Lokesh Kumar <kumarlokesh1991@outlook.com>
2022-11-18 15:37:44 +0000

    first commit

---
+++ src/main/java/com/codespotless/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.codespotless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+                                public static final String myConst = "we learn the spotless maven plugin for the code formatter";
+
+  public static void main(String[] args) {
+
+    System.out.
+            println("Diper Cipher");
+            System.out.println
+                    ("Testing Testing");
+
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
